5dcd0c8f016acb8dcb43bb7556688e390a8d4978
alchapman4061 <alchapman4061@eagle.fgcu.edu>
2019-05-16 14:36:08 -0400

    added menu

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << std::endl;
+    cout << "Production Line Tracker\n\n";
+    cout << "1. Add Employee Account\n";
+    cout << "2. Add Music Player\n";
+    cout << "3. Add Movie Player\n";
+    cout << "4. Display Production Statistics\n";
+    cout << "5. Exit\n";
     return 0;
 }
